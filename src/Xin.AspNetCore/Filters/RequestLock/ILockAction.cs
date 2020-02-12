59f52fbed7a26047adb4a2ad3124c4dc82e8f9be
huang <huangxinster@hotmail.com>
2020-02-12 16:31:18 +0800

    add requestlock

---
+++ src/Xin.AspNetCore/Filters/RequestLock/ILockAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Xin.AspNetCore.Filters.RequestLock
+{
+    public interface ILockAction
+    {
+        Task<bool> Lock(ActionExecutingContext context);
+
+        Task Unlock(ActionExecutingContext context);
+
+        Task ErrorResult(ActionExecutingContext context);
+
+    }
+}
---
+++ src/Xin.AspNetCore/Filters/RequestLock/RequestLockFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Xin.AspNetCore.Filters.RequestLock
+{
+    public class RequestLockFilter : Attribute, IAsyncActionFilter
+    {
+        private readonly Type _action;
+
+        public RequestLockFilter(Type action)
+        {
+            if (action.GetInterface(nameof(ILockAction)) == null)
+            {
+                throw new ArgumentException("Type must be ILockAction");
+            }
+
+            _action = action;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!(context.HttpContext.RequestServices.GetServices(typeof(ILockAction))
+                .FirstOrDefault(x => x.GetType() == _action) is ILockAction action))
+            {
+                throw new NullReferenceException("action is null");
+            }
+
+            if (await action.Lock(context))
+            {
+                await next();
+                await action.Unlock(context);
+            }
+            else
+            {
+                await action.ErrorResult(context);
+            }
+        }
+    }
+}
